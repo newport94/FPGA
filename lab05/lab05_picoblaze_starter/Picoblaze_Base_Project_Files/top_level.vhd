8e321470bf245a09278890eae35fbfad3776fe65
Ryan <rnewport20@gmail.com>
2018-10-21 20:17:54 -0400

    added picoblaze starter files

--- lab05/lab05_picoblaze_starter/Picoblaze_Base_Project_Files/top_level.vhd
+++ lab05/lab05_picoblaze_starter/Picoblaze_Base_Project_Files/top_level.vhd
@@ -1,61 +1,18 @@
 ----------------------------------------------------------------------------------
--- Company:    JHU EP
--- Engineer:   Ryan Newport
+-- Engineer:    K. Newlander
+-- Course:      ECE 525.442 JHU Engineering
+--
+-- Description: Lab Base Picoblaze Project which demonstrates the instatiation
+--              of a Picoblaze soft core processor on the Artix-7 FPGA
 -- 
--- Create Date:    10/21/2018 
--- Module Name:    lab05_top
--- Project Name:   Lab05
--- Target Device:  Xilinx Artix-7 xc7a100tcsg324-1
---
--- Description:  top level entity for lab 05 picoblaze
---------------------------------------------------------------------------------------
---  OPERATIONS
---    Result         : 16 bit result stored in two 8-bit registers (starts at zero)
---    First operand  : 4-bit switch input
---    Second operand : 16-bit value above
---    OP REG,SW
---
---  BUTTONS
---    BTN_R : initiates operation (one per press)
---    BTN_D : resets 16-bit reg value to zero
---
---  SWITCHES
---    SW[7..4] : operation to peform after BTN_R pressed
---         SW[7] : multiplication (NO wrap around)
---         SW[6] : left shift     (NO wrap around)
---         SW[5] : subtract       (wraps around)
---         SW[4] : add            (wraps around)
---    SW[3..0] : 4-bit operand input value
---
---  DISPLAY
---    7SEG[7..4] : 0x0000 or debugging
---    7SEG[3..0] : Displays 16-bit result
---
---  PRECEDENCE 
---    ADD > SUBTRACT > LEFT SHIFT > MULTIPLICATION
---
---  NEXYS-4 PORT IDs
---    case port_id is
---    when x"00"  => in_port <= status_register;
---    when x"01"  => in_port <= data_from_uartrx;
---    when x"02"  => in_port <= sliders;
---    when x"03"  => in_port <= x"0" & buttons;
---    when x"04"  => in_port <= seg7chars(7 downto 0);
---    when x"05"  => in_port <= seg7chars(15 downto 8);
---    when x"06"  => in_port <= leds_reg;
---    when x"07"  => in_port <= mstimer(7 downto 0);
---    when x"08"  => in_port <= mstimer(15 downto 8);
---    when others => in_port <= x"5a";
---    end case;
---
-------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------
+
 
 library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
-use work.all;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
-entity lab05_top is
+entity top_level is
     Port (  --Clock
           CLK100MHZ : in STD_LOGIC;
    
@@ -81,9 +38,9 @@ entity lab05_top is
            --Seg7 Display Signals
           SEG7_CATH : out STD_LOGIC_VECTOR (7 downto 0);
           AN : out STD_LOGIC_VECTOR (7 downto 0));
-end lab05_top;
+end top_level;
 
-architecture Behavioral of lab05_top is
+architecture Behavioral of top_level is
 
     --TODO: Package these into seperate file for ease of readibility
     
---
+++ lab05/lab05_picoblaze_starter/Picoblaze_Tools/README.txt
@@ -0,0 +1,6 @@
+Author:	K. Newlander
+
+This directory is used to generate the VHD ROM file for the Picoblaze Project.  
+Write the software using the picoblaze assembly language in the lab6_starter.psm file.  
+Then once ready to test, run the kcpsm6.exe and enter the file to process, which in this case is lab6_starter.psm.  
+Assuming no errors in the assemlbly the tool will generate the lab6_starter.vhd file which can be imported into the Xilinx Picoblaze project to generate a new bitfile with the new software.
