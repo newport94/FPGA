e41de790a9434f443dfab2e881e46fcc2d27abdd
Ryan <rnewport20@gmail.com>
2018-11-18 15:11:49 -0500

    threshold somewhat working

--- lab04/rtl/lab04_top.vhd
+++ lab04/rtl/lab04_top.vhd
@@ -36,6 +36,13 @@ entity lab04_top is
 end entity lab04_top;
  
 architecture rtl of lab04_top is
+
+  constant k_pos_thr_low  : unsigned(7 downto 0) := x"3A";
+  constant k_pos_thr_high : unsigned(7 downto 0) := x"60";  
+  constant k_neg_thr_low  : unsigned(7 downto 0) := x"C0";
+  constant k_neg_thr_high : unsigned(7 downto 0) := x"A0";
+
+
  
   signal w_db_up, w_db_left, w_db_right, w_db_down : std_logic;
   signal w_rst, w_mode : std_logic;
@@ -44,7 +51,11 @@ architecture rtl of lab04_top is
   signal w_data_ad, w_data_1d, w_data_x, w_data_y, w_data_z : std_logic_vector(7 downto 0);
   signal w_disp : std_logic_vector(1 downto 0);
   signal w_char4, w_char5, w_char6, w_char7  : std_logic_vector (3 downto 0);
+  signal w_mv_up, w_mv_left, w_mv_right, w_mv_down : std_logic;
   
+  signal q_accel_left, q_accel_right, q_accel_up, q_accel_down : std_logic;
+  signal d_accel_left, d_accel_right, d_accel_up, d_accel_down : std_logic;  
+  signal w_accel_left, w_accel_right, w_accel_up, w_accel_down : std_logic;    
   
   
 
@@ -56,15 +67,47 @@ begin
   w_db_right  <= w_db_array(2);
   w_db_down   <= w_db_array(3);
   w_mode      <= SW(1);
-  w_disp      <= SW(4 downto 3); -- controls digits 4 - 7
+  w_disp      <= SW(4 downto 3); -- controls digits 7 - 4
                                     -- '00' --> display 1D in digits 7-6 and AD in 5-4
                                     -- '01' --> display x data in digits 5-4, zeros in 7-6
                                     -- '10' --> display y data in digits 5-4, zeros in 7-6
                                     -- '11' --> dispaly z data in digits 5-4, zeros in 7-6
                                     
+  w_mv_left  <= w_accel_left  when (w_mode = '1') else w_db_up   ;
+  w_mv_right <= w_accel_right when (w_mode = '1') else w_db_left ;                              
+  w_mv_up    <= w_accel_up    when (w_mode = '1') else w_db_right;                             
+  w_mv_down  <= w_accel_down  when (w_mode = '1') else w_db_down ;
+                                
+  -- set accel thresholds for moving red square
+  -- positive threshold =  > 0x03
+  -- negative threshold =  < 0x0D
+  d_accel_left  <= '1' when ((unsigned(w_data_y) > k_pos_thr_low) AND (unsigned(w_data_y) < k_pos_thr_high)) else '0';
+  d_accel_right <= '1' when ((unsigned(w_data_y) < k_neg_thr_low) AND (unsigned(w_data_y) > k_neg_thr_high)) else '0';
+  d_accel_up    <= '1' when ((unsigned(w_data_x) < k_neg_thr_low) AND (unsigned(w_data_x) > k_neg_thr_high)) else '0';
+  d_accel_down  <= '1' when ((unsigned(w_data_x) > k_pos_thr_low) AND (unsigned(w_data_x) < k_pos_thr_high)) else '0';
+  
+  w_accel_left  <= d_accel_left  AND NOT q_accel_left;
+  w_accel_right <= d_accel_right AND NOT q_accel_right;
+  w_accel_up    <= d_accel_up    AND NOT q_accel_up;
+  w_accel_down  <= d_accel_down  AND NOT q_accel_down;
+  
+  S_accel_thresh : process(CLK100MHZ, w_rst)
+  begin
+    if (w_rst = '1') then 
+      q_accel_left  <= '0';
+      q_accel_right <= '0';
+      q_accel_up    <= '0';
+      q_accel_down  <= '0';
+    elsif (rising_edge(CLK100MHZ)) then 
+      q_accel_left  <= d_accel_left ;
+      q_accel_right <= d_accel_right;
+      q_accel_up    <= d_accel_up   ;
+      q_accel_down  <= d_accel_down ;    
+    end if;
+  end process S_accel_thresh;
+                                    
                                     
   -- select char signals with w_disp
-  
   C_char_sel : process(w_mode, w_disp, w_data_ad, w_data_1d, w_data_x, w_data_y, w_data_z)
   begin
     if (w_mode = '1') then
@@ -75,13 +118,13 @@ begin
           w_char6 <= w_data_1d(3 downto 0);
           w_char7 <= w_data_1d(7 downto 4);
         when "01" =>
-          w_char4 <= w_data_x(3 downto 0);
-          w_char5 <= w_data_x(7 downto 4);
+          w_char4 <= w_data_y(3 downto 0);--w_data_x(3 downto 0);   -- x and y appear to be opposite of expected relative to 7 segment display
+          w_char5 <= w_data_y(7 downto 4);--w_data_x(7 downto 4);   -- so I switched them 
           w_char6 <= x"0";
           w_char7 <= x"0";
         when "10" =>
-          w_char4 <= w_data_y(3 downto 0);
-          w_char5 <= w_data_y(7 downto 4);
+          w_char4 <= w_data_x(3 downto 0);--w_data_y(3 downto 0);
+          w_char5 <= w_data_x(7 downto 4);--w_data_y(7 downto 4);
           w_char6 <= x"0";
           w_char7 <= x"0";
         when "11" =>
@@ -124,10 +167,10 @@ begin
     Port map(
       i_clk      => CLK100MHZ,
       i_rst      => w_rst,
-      i_db_up    => w_db_up,
-      i_db_left  => w_db_left,
-      i_db_right => w_db_right,
-      i_db_down  => w_db_down,
+      i_db_up    => w_mv_up,
+      i_db_left  => w_mv_left,
+      i_db_right => w_mv_right,   
+      i_db_down  => w_mv_down, 
       o_vga_red  => VGA_R, 
       o_vga_grn  => VGA_G,
       o_vga_blu  => VGA_B,
@@ -161,33 +204,4 @@ begin
       o_db  => w_db_array(ii));
   end generate GEN_PUSH_BUTTONS;
         
-        
-  -- UP_BUTTON : entity work.debounce(rtl) 
-    -- Port map(
-      -- i_clk => CLK100MHZ,   -- 100 MHz clk
-      -- i_rst => w_rst,
-      -- i_pb  => BTNU,
-      -- o_db  => w_db_up);
-      
-  -- LEFT_BUTTON : entity work.debounce(rtl) 
-    -- Port map(
-      -- i_clk => CLK100MHZ,   -- 100 MHz clk
-      -- i_rst => w_rst,
-      -- i_pb  => BTNL,
-      -- o_db  => w_db_left);
-
-  -- RIGHT_BUTTON : entity work.debounce(rtl) 
-    -- Port map(
-      -- i_clk => CLK100MHZ,   -- 100 MHz clk
-      -- i_rst => w_rst,
-      -- i_pb  => BTNR,
-      -- o_db  => w_db_right);
-
-  -- DOWN_BUTTON : entity work.debounce(rtl) 
-    -- Port map(
-      -- i_clk => CLK100MHZ,   -- 100 MHz clk
-      -- i_rst => w_rst,
-      -- i_pb  => BTND,
-      -- o_db  => w_db_down);
-    
  end architecture rtl;
